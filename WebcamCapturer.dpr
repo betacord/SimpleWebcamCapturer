8e16b60ab22b516a9bf6e9af95cb69143d03bd4a
Tomasz Krzywicki <t-krzywicki@wp.pl>
2017-08-22 22:12:34 +0200

    initial commit

---
+++ WebcamCapturer.dpr
@@ -0,0 +1,14 @@
+program WebcamCapturer;
+
+uses
+  Vcl.Forms,
+  uMain in 'uMain.pas' {fMain};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TfMain, fMain);
+  Application.Run;
+end.
